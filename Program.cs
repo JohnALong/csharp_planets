4bd48dd277f929589c557a9017253759cd3e3f00
John Long <jlcomp45@gmail.com>
2020-03-27 08:32:33 -0500

    working on planet list exercise

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+            List<string> lastPlanets = new List<string>(){"Uranus", "Neptune"};
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            planetList.AddRange(lastPlanets);
+
+            foreach (string planet in planetList) {
+                Console.WriteLine(planet);
+            }
+
+            foreach (string lastPlanet in lastPlanets) {
+                Console.WriteLine(lastPlanet);
+            }
+
+            Console.WriteLine(String.Join(", ", planetList));
+        } 
+    }
+}
