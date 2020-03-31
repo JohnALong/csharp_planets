2ce73bfdfcf84b694e58917442ba4fce7dfc218e
John Long <jlcomp45@gmail.com>
2020-03-31 02:05:41 -0500

    completed planets practice

--- Program.cs
+++ Program.cs
@@ -7,23 +7,33 @@ namespace lists
     {
         static void Main(string[] args)
         {
+            List<string> rockyPlanets = new List<string>();
             List<string> planetList = new List<string>(){"Mercury", "Mars"};
             List<string> lastPlanets = new List<string>(){"Uranus", "Neptune"};
-
+    
             planetList.Add("Jupiter");
             planetList.Add("Saturn");
 
             planetList.AddRange(lastPlanets);
+            planetList.Insert(1,"Venus");
+            planetList.Insert(2,"Earth");
+            planetList.Add("Pluto");
 
-            foreach (string planet in planetList) {
-                Console.WriteLine(planet);
-            }
+            rockyPlanets = planetList.GetRange(0,4);
 
-            foreach (string lastPlanet in lastPlanets) {
-                Console.WriteLine(lastPlanet);
-            }
+            planetList.Remove("Pluto");
+            // foreach (string planet in planetList) {
+            //     Console.WriteLine(planet);
+            // }
+
+            // foreach (string lastPlanet in lastPlanets) {
+            //     Console.WriteLine(lastPlanet);
+            // }
 
             Console.WriteLine(String.Join(", ", planetList));
-        } 
+            foreach (string rockyplanet in rockyPlanets) {
+                Console.WriteLine(rockyplanet);
+            }
+         }   
     }
 }
