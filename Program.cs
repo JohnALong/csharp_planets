2ca35087af260ef5a43d34a8c2017bc67b9b084b
John Long <jlcomp45@gmail.com>
2020-03-31 14:10:01 -0500

    completed random numbers

--- Program.cs
+++ Program.cs
@@ -22,18 +22,39 @@ namespace lists
             rockyPlanets = planetList.GetRange(0,4);
 
             planetList.Remove("Pluto");
-            // foreach (string planet in planetList) {
-            //     Console.WriteLine(planet);
-            // }
+            foreach (string planet in planetList) {
+                Console.WriteLine(planet);
+            }
 
-            // foreach (string lastPlanet in lastPlanets) {
-            //     Console.WriteLine(lastPlanet);
-            // }
+            foreach (string lastPlanet in lastPlanets) {
+                Console.WriteLine(lastPlanet);
+            }
 
             Console.WriteLine(String.Join(", ", planetList));
             Console.WriteLine(String.Join(", ", rockyPlanets));
             foreach (string rockyplanet in rockyPlanets) {
                 Console.WriteLine(rockyplanet);
+            }
+            Random random = new Random();
+            List<int> numbers = new List<int> {
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+            };
+            for (int i=0; i<numbers.Count; i++) 
+            {
+                // Determine if the current loop index is contained inside of the `numbers` list. Print a message to the console indicating whether the index is in the list.
+                if (numbers.Contains(i))
+                {
+                    Console.WriteLine($"Numbers list contains {i}");
+                }
+                else
+                {
+                    Console.WriteLine($"Numbers list does not contain {i}");
+                }
             }
          }   
     }
